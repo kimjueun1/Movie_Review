b8478d725405934721088bc9f825208e03b4c72e
kimjueun1 <kje9862@naver.com>
2021-12-09 23:58:25 +0900

    init

---
+++ SpringBoard4/src/main/java/com/myspring/myspring/BoardController.java
@@ -0,0 +1,55 @@
+package com.myspring.myspring;
+
+import java.text.DateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class BoardController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+//	
+	/**
+	 * Simply selects the home view to render by returning its name.
+	 */
+//	@RequestMapping(value = "/boardlist", method = RequestMethod.GET)
+//	public String home(Locale locale, Model model) {
+//		logger.info("Welcome home! The client locale is {}.", locale);
+//		
+//		Date date = new Date();
+//		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+//		
+//		String formattedDate = dateFormat.format(date);
+//		
+//		model.addAttribute("serverTime", formattedDate );
+//		
+//		return "board/list";
+//	}
+	@RequestMapping(value = "/boardlist", method = RequestMethod.GET)
+	public String boardList(Locale locale, Model model) {
+		logger.info("Welcome home! The client locale is {}.", locale);
+		
+		Date date = new Date();
+		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+		
+		String formattedDate = dateFormat.format(date);
+		
+		model.addAttribute("serverTime", formattedDate );
+		
+		List<String> posts = new ArrayList<>();
+		posts.add("오늘은 벌써 15주차 목요일 ");
+		model.addAttribute("list", posts);
+		
+		return "board/list";
+	}
+	
+}
